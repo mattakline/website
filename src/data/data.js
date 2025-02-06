6de03a69bca0ff099aae8df9b055322484a9563d
Matt Kline <matt@Matts-MacBook-Air.local>
2025-02-06 18:41:27 -0500

    Added video links

--- src/data/data.js
+++ src/data/data.js
@@ -44,14 +44,14 @@ export const agencies = [
 export const dishes = [
   {
     id: 1,
-    line0: "Brand",
+    line0: "Broadcast",
     line1: "ESL",
     line2: "PRO LEAGUE",
-    line3: "Director _ Matt Kline",
-    line4: "Production _ Capacity",
+    line3: "EP _ Matt Kline",
+    line4: "Producer _ Matt Kline",
     line5: "Agency _ Turtle ",
     line6: "",
-    link: "https://links.unsplash.com/photo-1707576517985-a2e912cca325?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    link: "https://www.youtube.com/watch?v=cqWABHJxNy4"
   },
   {
     id: 2,
@@ -73,18 +73,18 @@ export const dishes = [
     line4: "Production _ Engine",
     line5: "Agency _ Microsoft",
     line6: "",
-    link: "https://links.unsplash.com/photo-1633368475182-7c4773552630?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    link: "https://youtu.be/oZkz8w1JOtk?t=726"
   },
   {
     id: 4,
     line0: "Series",
     line1: "XBOX",
     line2: "NO PLAYING FIELD",
-    line3: "EP _ Matt Kline",
+    line3: "Creator _ Matt Kline",
     line4: "Production _ Matt Kline",
     line5: "Agency _ Mashable",
     line6: "",
-    link: "https://links.unsplash.com/photo-1626844131082-256783844137?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    link: "https://www.youtube.com/watch?v=UQDGS9Qvh8g"
   },
   {
     id: 5,
@@ -95,7 +95,7 @@ export const dishes = [
     line4: "Edit _ Riley Hastings",
     line5: "Agency _ Microsoft",
     line6: "",
-    link: "https://www.youtube.com/watch?v=q3fa-OIsBvU"
+    link: "https://www.youtube.com/watch?v=6l5t_Acxuic"
   },
   {
     id: 6,
@@ -106,7 +106,7 @@ export const dishes = [
     line4: "Production _ Matt Kline",
     line5: "Agency _ Mashable",
     line6: "",
-    link: "https://links.unsplash.com/photo-1597652096872-658bf24731ec?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    link: "https://www.youtube.com/watch?v=jAu1ZsTCA64"
   },
   {
     id: 7,
@@ -117,7 +117,7 @@ export const dishes = [
     line4: "Production _ Loose",
     line5: "Agency _ Microsoft",
     line6: "",
-    link: "https://links.unsplash.com/photo-1597652096872-658bf24731ec?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    link: "https://www.youtube.com/watch?v=43k__JLKxqw"
   },
   {
     id: 8,
@@ -133,24 +133,24 @@ export const dishes = [
   {
     id: 9,
     line0: "Broadcast",
-    line1: "ESL",
-    line2: "EPL LONDON",
+    line1: "Intel",
+    line2: "IEM Katowice",
     line3: "Producer _ Matt Kline", 
-    line4: "Director _ Matt Kline",
+    line4: "EP _ Simon Eicher",
     line5: "Agency _ Turtle",
     line6: "",
-    link: "https://links.unsplash.com/photo-1597652096872-658bf24731ec?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    link: "https://youtu.be/oZkz8w1JOtk?t=24427"
   },
   {
     id: 10,
-    line0: "Series",
-    line1: "OLD SPICE",
-    line2: "IEM KATOWICE",
+    line0: "Ad",
+    line1: "ASTRO",
+    line2: "EVENT EXPERIENCE",
     line3: "EP _ Matt Kline",
-    line4: "Production _ Matt Kline",
-    line5: "Agency _ Mashable",
+    line4: "Production _ Loose",
+    line5: "Agency _ Microsoft",
     line6: "",
-    link: "https://links.unsplash.com/photo-1597652096872-658bf24731ec?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    link: "https://www.youtube.com/watch?v=-Z2uSkUBanA"
   },
   {
     id: 11,
@@ -161,7 +161,7 @@ export const dishes = [
     line4: "EP _ Simon Eicher",
     line5: "Agency _ Turtle",
     line6: "",
-    link: "https://links.unsplash.com/photo-1597652096872-658bf24731ec?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    link: "https://www.youtube.com/watch?v=QgYOeuvuOas"
   },
   {
     id: 12,
@@ -171,7 +171,8 @@ export const dishes = [
     line3: "Director _ Matt Kline",
     line4: "Production _ Capacity",
     line5: "Agency _ 343",
-    line6: "",
-    link: "https://links.unsplash.com/photo-1597652096872-658bf24731ec?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    line6: "https://www.youtube.com/@HCS/streams",
+    link: "https://youtu.be/nN-4jfcluKQ&t=885"
   }
 ];
+// IEM https://www.youtube.com/watch?v=KySzKy9NTUk
